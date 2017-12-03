680f0e6af15564a70a8fb52adac977cc850b105f
Manthan Thakar <manthant15@gmail.com>
2017-12-03 16:00:42 -0500

    Add rewrite component in the list of components

--- src/main/scala/org/so/plugin/JoinOptimizer.scala
+++ src/main/scala/org/so/plugin/JoinOptimizer.scala
@@ -2,12 +2,13 @@ package main.scala.org.so.plugin
 
 import scala.tools.nsc.Global
 import scala.tools.nsc.plugins.Plugin
-import components.AnalysisComponent
+import components.{AnalysisComponent, RewriteComponent}
 
 class JoinOptimizer(val global: Global) extends Plugin {
 
   override val name = "JoinOptimizer"
   override val description = "Optimize Spark joins for all actions"
   val analysisComponent = new AnalysisComponent(global, name)
-  override val components = List(analysisComponent)
+  val rewriteComponent = new RewriteComponent(global, name)
+  override val components = List(analysisComponent, rewriteComponent)
 }
