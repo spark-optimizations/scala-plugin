a84570f28ddd5571564089cc9e71c7164d4fe460
Shabbir Hussain <shabbir.ahussain@gmail.com>
2017-12-03 15:33:27 -0500

    Adding test cases for lambda matching

--- src/test/scala/org/so/plugin/BadMapSparkTest_3.scala
+++ src/test/scala/org/so/plugin/BadMapSparkTest_3.scala
@@ -1,25 +1,24 @@
 package org.so.plugin
 
-import java.text.SimpleDateFormat
-
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
   * @author shabbir.ahussain
   */
-object BadMapSparkTest {
-
+object BadMapSparkTest_3
+{
     def main(args: Array[String]) {
-      val conf = new SparkConf().setAppName("Song High Fidelity").setMaster("local")
+      val conf = new SparkConf().setAppName("Bad Test 2")
+        .setMaster("local")
       val sc = new SparkContext(conf)
-
 
       val rdda = sc.parallelize(List((1, (1, 11)), (2, (2, 22))))
       val rddb = sc.parallelize(List((1, (1, 11)), (2, (2, 22))))
 
       rdda
           .join(rddb)
-          .mapValues[Int](x=> x._1._1 + x._1._2)
+          .mapValues[Int]{case (x:((Int, Int), (Int, Int)))
+              => x._1._1 + x._2._2}
           .collect()
     }
   }
