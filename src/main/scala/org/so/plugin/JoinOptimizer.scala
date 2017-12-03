6f7ddb3256ee354823df9f98de3d5ffcd32c291e
Manthan Thakar <manthant15@gmail.com>
2017-12-03 15:37:27 -0500

    Cleanup JoinOptimizer plugin class

--- src/main/scala/org/so/plugin/JoinOptimizer.scala
+++ src/main/scala/org/so/plugin/JoinOptimizer.scala
@@ -1,18 +1,15 @@
-package org.so.plugin
+package main.scala.org.so.plugin
 
-import scala.tools.nsc.{Global, Phase}
-import scala.tools.nsc.plugins.{Plugin, PluginComponent}
+import org.so.plugin.analysis.LambdaAnalyzer
+
+import scala.tools.nsc.Global
+import scala.tools.nsc.plugins.Plugin
+import components.AnalysisComponent
 
 class JoinOptimizer(val global: Global) extends Plugin {
-  override val name = "joinOptimizer"
-  override val description = "Optimize Spark join for all actions"
-  override val components = List[PluginComponent](Component)
 
-  private object Component extends PluginComponent {
-    override val global: Global = JoinOptimizer.this.global
-    override val phaseName: String = JoinOptimizer.this.name
-    override val runsAfter: List[String] = List[String]("refchecks")
-
-    override def newPhase(prev: Phase): Phase = ???
-  }
+  override val name = "JoinOptimizer"
+  override val description = "Optimize Spark joins for all actions"
+  val analysisComponent = new AnalysisComponent(global, name)
+  override val components = List(analysisComponent)
 }
