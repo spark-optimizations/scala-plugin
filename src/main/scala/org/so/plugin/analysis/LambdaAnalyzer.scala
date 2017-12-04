09204a71b9ccff930dbf353551c5d1f6a4d389e2
Shabbir Hussain <shabbir.ahussain@gmail.com>
2017-12-03 20:23:22 -0500

    Added map filter support

--- src/main/scala/org/so/plugin/analysis/LambdaAnalyzer.scala
+++ src/main/scala/org/so/plugin/analysis/LambdaAnalyzer.scala
@@ -25,30 +25,46 @@ class LambdaAnalyzer(val global: Global) {
     }
   }
 
-
   /** Finds the usages of the given parameter.
     *
     * @param tree  is the function body tree to parse.
     * @param param is the parameter to scan for.
-    * @param fnType is the speark RDD transformation to optimize for.
+    * @param fnName is the speark RDD transformation to optimize for.
     * @return A tuple of fields used in left and right side of tuple. If empty then none of the fields are used from that tuple.
     */
-  private def findParamUsage(tree: Tree, param: String, fnType: String)
+  private def findParamUsage(tree: Tree, param: String, fnName: String)
   : (Set[String], Set[String]) = {
+    /** Finds the usages of the given parameter and tuple position.
+      *
+      * @param tree is the function body tree to parse.
+      * @param pos  is the position to look for within tuple.
+      */
+    def fposMapValues(tree: Tree, pos: String): Set[String] = tree match {
+      case Select(Select(Ident(TermName(`param`)), TermName(`pos`)), TermName(idx)) =>
+        Set(idx)
+      case a =>
+        a.children
+          .flatMap(x => fposMapValues(x, pos))
+          .toSet
+    }
 
     /** Finds the usages of the given parameter and tuple position.
       *
       * @param tree is the function body tree to parse.
       * @param pos  is the position to look for within tuple.
       */
-    def findParamPosUsage(tree: Tree, pos: String): Set[String] = tree match {
-      case Select(Select(Ident(TermName(`param`)), TermName(`pos`)), a@TermName(idx)) =>
+    def fposMap(tree: Tree, pos: String): Set[String] = tree match {
+      case Select(Select(Select(Ident(TermName(`param`)), TermName(`pos`)), TermName("_2")), TermName(idx)) =>
         Set(idx)
       case a =>
         a.children
-          .flatMap(x => findParamPosUsage(x, pos))
+          .flatMap(x => fposMap(x, pos))
           .toSet
     }
-    (findParamPosUsage(tree, "_1"), findParamPosUsage(tree, "_2"))
+
+    fnName match {
+      case "mapValues" => (fposMapValues(tree, "_1"), fposMapValues(tree, "_2"))
+      case "map" | "filter" => (fposMap(tree, "_1"), fposMap(tree, "_2"))
+    }
   }
 }
